5a8b57ac11d9bb2de7b352b92706619f228e1b0a
Yarin9Ashmoz <154062144+Yarin9Ashmoz@users.noreply.github.com>
2026-03-03 15:54:02 +0200

    Use SQLite DB for Render

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
