4fa55eea20c3c18478ded5f4f72f370fbbcd0f97
Yarin9Ashmoz <154062144+Yarin9Ashmoz@users.noreply.github.com>
2026-04-25 16:10:46 +0300

    new database url updeted

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://aquamind_db_user:sacIscY7YjGbMZINkLAoLCliWCp8Volu@dpg-d6k18dh4tr6s73bef8o0-a.oregon-postgres.render.com/aquamind_db"
+DATABASE_URL = "postgresql://aquamind_db_2l4g_user:nnQeoqaaupN9A02hRCh0Ce2tTG9Ajz4w@dpg-d7mbohapmmbs73btb720-a/aquamind_db_2l4g"
 
 engine = create_engine(DATABASE_URL)
 
